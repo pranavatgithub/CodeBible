b82fb19e623ff09e2a82b8236726044a768f960b
pranav <pranavr.karthika@gmail.com>
2018-03-25 14:08:17 +0530

    flexbox and js design patterns

--- code samples/js & jQuery/Design Pattern.js
+++ code samples/js & jQuery/Design Pattern.js
@@ -1,6 +1,6 @@
 //Delegation-Oriented Design
 
-var Task = {  // our base class(class really?)
+var Task = { // our base class(class really?)
     setID: function (ID) {
         this.id = ID;
     },
@@ -33,12 +33,55 @@ XXX.setID("001");
 
 XXX.outputID(); // similarly it won't affect XYZ as well
 
-XYZ.outputID = function (){ //overriden base class methods (polymorphism)
-    console.log(this.id  + ' overrdiden');
+XYZ.outputID = function () { //overriden base class methods (polymorphism)
+    console.log(this.id + ' overrdiden');
 }
 XYZ.outputID();
 
-Task.newMethod = function(){
+Task.newMethod = function () {
     console.log("new method added to super class");
 }
 XYZ.newMethod(); // got it no issue so far.
+
+
+// Prototypical 
+
+function Foo(who) {
+    this.me = who;
+}
+Foo.prototype.identify = function () {
+    return "I am " + this.me;
+};
+
+function Bar(who) {
+    Foo.call(this, who);
+}
+Bar.prototype = Object.create(Foo.prototype);
+Bar.prototype.speak = function () {
+    alert("Hello, " + this.identify() + ".");
+};
+var b1 = new Bar("b1");
+var b2 = new Bar("b2");
+b1.speak();
+b2.speak();
+
+// simplified form via OLOO (Object Linked to Other Object)
+
+var Foo = {
+    init: function (who) {
+        this.me = who;
+    },
+    identify: function () {
+        return "I am " + this.me;
+    }
+};
+var Bar = Object.create(Foo);
+Bar.speak = function () {
+    console.log("Hello, " + this.identify() + ".");
+};
+var b1 = Object.create(Bar);
+b1.init("b1");
+var b2 = Object.create(Bar);
+b2.init("b2");
+b1.speak();
+b2.speak();
