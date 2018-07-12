01ae907f7906ad706b65c6d88870109e777d0afb
pranav <pranavr.karthika@gmail.com>
2018-07-13 01:56:10 +0530

    angular ,css ,sass

--- code samples/Angular/variables.ts
+++ code samples/Angular/variables.ts
@@ -1,5 +1,6 @@
 export class UserItemComponent implements OnInit {
     name: string; // <-- added name property - we can render it like {{name}} in html
+    title: HTMLInputElement;
     constructor() {
         this.name = 'Felipe'; // set the name
     }
