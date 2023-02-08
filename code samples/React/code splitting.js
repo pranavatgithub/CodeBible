f409c3c30c1448e2fab0d1fd0ce98baa2fcd1fbf
Pranav R <pranavr.karthika@gmail.com>
2023-02-08 15:36:17 +0530

    Create code splitting.js

---
+++ code samples/React/code splitting.js
@@ -0,0 +1,13 @@
+import React, { Suspense } from 'react';
+
+const OtherComponent = React.lazy(() => import('./OtherComponent'));
+
+function MyComponent() {
+  return (
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <OtherComponent />
+      </Suspense>
+    </div>
+  );
+}
