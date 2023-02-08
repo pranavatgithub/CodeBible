364e4dcc3d5fa15e3f2fe1a8ac3eef974bfecd6d
Pranav R <pranavr.karthika@gmail.com>
2023-02-08 15:41:05 +0530

    Update code splitting.js

--- code samples/React/code splitting.js
+++ code samples/React/code splitting.js
@@ -11,3 +11,23 @@ function MyComponent() {
     </div>
   );
 }
+
+
+// router based
+
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const Home = lazy(() => import('./routes/Home'));
+const About = lazy(() => import('./routes/About'));
+
+const App = () => (
+  <Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Suspense>
+  </Router>
+);
