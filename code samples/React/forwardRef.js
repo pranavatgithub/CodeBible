eba3d15e2449233da1281f447e00aa89e01357bf
Pranav R <pranavr.karthika@gmail.com>
2023-02-08 15:46:16 +0530

    Create forwardRef.js

---
+++ code samples/React/forwardRef.js
@@ -0,0 +1,33 @@
+import { forwardRef } from 'react';
+
+
+// child
+const MyInput = forwardRef(function MyInput(props, ref) { // the parent can access child using this ref
+  const { label, ...otherProps } = props;
+  return (
+    <label>
+      {label}
+      <input {...otherProps} ref={ref} />
+    </label>
+  );
+});
+
+
+// parent
+
+function Form() {
+  const ref = useRef(null);
+
+  function handleClick() {
+    ref.current.focus();
+  }
+
+  return (
+    <form>
+      <MyInput label="Enter your name:" ref={ref} />
+      <button type="button" onClick={handleClick}>
+        Edit
+      </button>
+    </form>
+  );
+}
